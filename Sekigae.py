887df1e606c38fe15956d8e764af6d2684dfb2fc
Iruyan_Zak <yuri.kazuna@gmail.com>
2016-12-15 08:47:25 +0900

    Please fix Line:72

--- Sekigae.py
+++ Sekigae.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 import ast
 
@@ -12,23 +12,28 @@ class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     rows = db.Column(db.Integer)
     columns = db.Column(db.Integer)
-    seats = db.relationship('seat', backref='room')
+    unused_seats = db.Column(db.PickleType)
+#     seats = db.relationship('seat', backref='room')
     commands = db.relationship('Command', backref='room')
 
-    def __init__(self, rows, columns):
+    def __init__(self, rows, columns, unused_seats):
         self.rows = rows
         self.columns = columns
+        self.unused_seats = unused_seats
 
 
-class Seat(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    room_id = db.Column(db.Integer, db.ForeignKey('room.id'))
-    person = db.relationship('Person', backref='seat', uselist=False)
-
+# class Seat(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     room_id = db.Column(db.Integer, db.ForeignKey('room.id'))
+#     index = db.Column(db.Integer)
+#     person = db.relationship('Person', backref='seat', uselist=False)
+#
+#     def __init__(self, index):
+#         self.index = index
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    seat_id = db.Column(db.Integer, db.ForeignKey('seat.id'))
+#     seat_id = db.Column(db.Integer, db.ForeignKey('seat.id'))
     index = db.Column(db.Integer)
     name = db.Column(db.String(32))
 
@@ -59,37 +64,42 @@ def create():
     if request.method == 'GET':
         return render_template('create.html')
 
-    width = request.form['width']
-    height = request.form['height']
+    print(request.form)
+    rows = request.form['rows']
+    columns = request.form['columns']
+    abort(404)
+    # 下の行で 400 Bad request が出る。
     unused_seats = request.form['unused_seats']
     positions = request.form['positions']
 
     # try:
-    width = int(width)
-    height = int(height)
+    rows = int(rows)
+    columns = int(columns)
     positions = iter(positions.split('\n'))
     unused_seats = ast.literal_eval(unused_seats)
-    room = Room(width, height)
+    room = Room(rows, columns, unused_seats)
     db.session.add(room)
-    for i in range(0, width * height):
+    for i in range(0, rows * columns):
         if i not in unused_seats:
-            seat = Seat()
-            seat.room_id = room.id
+            # seat = Seat(i)
+            # seat.room_id = room.id
             person = Person(i, next(positions))
-            person.seat_id = seat.id
+            person.room_id = room.id
             db.session.add(person)
-            db.session.add(seat)
+            # db.session.add(seat)
 
     db.session.commit()
     # except:
     #     return redirect('/create')
 
-    return redirect(url_for('/{0}'.format(room.id)))
+    return redirect(url_for('/rooms/{0}'.format(room.id)))
 
 
 @app.route('/rooms/<room_id>')
 def rooms(room_id):
-    Room.query.filter_by(id=room_id).first()
+    room = Room.query.filter_by(id=room_id).first()
+    persons = Person.query.filter_by(room_id=room.id).all().sort(lambda p: p.index)
+    return render_template("rooms.html", room=room, persons=persons)
 
 if __name__ == '__main__':
     app.run()
